5b83b7d53075700e6f3db8d99868912225978f43
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2020-10-09 16:25:03 +0800

    Update xv_training.py

--- xv_training.py
+++ xv_training.py
@@ -1,18 +1,11 @@
-import os
-import sys
-import glob
-import time
-import socket
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
-import torch.multiprocessing as mp
 from torch.utils.data import DataLoader
 import random
 from tqdm import tqdm
-
-from data_load import MFCC_24
+import argparse
+from data_load import MFCC_data
 from x_vector_recipe import xvecTDNN
 
 def setup_seed(seed):
@@ -21,68 +14,94 @@ def setup_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
-setup_seed(0)
-
-batch_size = 2048
-num_epochs = 10  #more
-learning_rate = 0.001
-
-#== train model ==
-# use_gpu = torch.cuda.is_available()
-model = xvecTDNN(feature_dim=24,num_lang=10,p_dropout=0.2)
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-multi_gpu = True
-if multi_gpu:
-    model = nn.DataParallel(model, device_ids=[0, 1, 2, 3])
-model.to(device)
-
-train_txt = '/home/hexin/Desktop/hexin/dataset/OLR2020/chunk/2017train/training_data.txt'
-test_txt = '/home/hexin/Desktop/hexin/dataset/OLR2020/chunk/2017test/testing_data.txt'
-train_set = MFCC_24(train_txt)
-test_set = MFCC_24(test_txt)
-
-train_data = DataLoader(
-    dataset = train_set,
-    batch_size = batch_size,
-    shuffle = True)
-test_data = DataLoader(
-    dataset = test_set,
-    batch_size = batch_size,
-    shuffle = False,
-)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-#optimizer = nn.DataParallel(optimizer, device_ids=[0, 1, 2, 3])
-
 
 def update_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
 
-# Train the model
-total_step = len(train_data)
-curr_lr = learning_rate
-for epoch in tqdm(range(num_epochs)):
-    for step, (utt, labels) in enumerate(train_data):
-        utt_ = utt.to(device)
-        labels = labels.long().to(device)
+def main():
+    parser = argparse.ArgumentParser(description='paras for making data')
+    parser.add_argument('--dim', type=int, help='dim of input features',
+                        default=20)
+    parser.add_argument('--modeldir', type=str, help='dir to save model',
+                        default='/home/hexin/Desktop/hexin/LID_project/LID_torch/model_NRF.ckpt')
+    parser.add_argument('--data', type=str, help='training data, in .txt')
+    parser.add_argument('--batch', type=int, help='batch size',
+                        default=2048)
+    parser.add_argument('--epoch',type=int, help='num of epochs',
+                        default=20)
+    parser.add_argument('--lang',type=int, help='num of language classes',
+                        default=10)
+    parser.add_argument('--lr',type=float,help='initial learning rate',
+                        default=0.001)
+    parser.add_argument('--multigpu', type=bool, help='True if use multiple GPUs to train',
+                        default=True)
+    args = parser.parse_args()
 
-        # Forward pass
-        outputs = model(utt_) # output <=> prerdict_train
-        loss = criterion(outputs, labels)
-        # metric = metric_func(outputs, labels)
+    setup_seed(0)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    batch_size = args.batch
+    num_epochs = args.epoch  #more
+    learning_rate = args.lr
 
-        if step % 10 == 0:
-            print("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
-                  .format(epoch + 1, num_epochs, step + 1, total_step, loss.item()))
-            # print("Metric:{.4f}".format(metric.item()/step))
-print(model.state_dict().keys())
-torch.save(model.state_dict(), 'model.ckpt')
+    #== train model ==
+    # use_gpu = torch.cuda.is_available()
+    model = xvecTDNN(feature_dim=args.dim,num_lang=10,p_dropout=0.2)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    multi_gpu = args.multigpu
+    if multi_gpu:
+        model = nn.DataParallel(model, device_ids=[0, 1, 2, 3])
+    model.to(device)
+
+    train_txt = args.data
+    # test_txt = '/home/hexin/Desktop/hexin/dataset/OLR2020/chunk/save_txt/testing_data.txt'
+    train_set = MFCC_data(train_txt)
+    # test_set = MFCC_data(test_txt)
+
+    train_data = DataLoader(
+        dataset = train_set,
+        batch_size = batch_size,
+        pin_memory=True,
+        num_workers=16,
+        shuffle = True)
+    # test_data = DataLoader(
+    #     dataset = test_set,
+    #     batch_size = batch_size,
+    #     shuffle = False,
+    # )
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    #optimizer = nn.DataParallel(optimizer, device_ids=[0, 1, 2, 3])
+
+    # Train the model
+    total_step = len(train_data)
+    curr_lr = args.lr
+    for epoch in tqdm(range(num_epochs)):
+        for step, (utt, labels) in enumerate(train_data):
+            utt_ = utt.to(device)
+            labels = labels.long().to(device)
+
+            # Forward pass
+            outputs = model(utt_) # output <=> prerdict_train
+            loss = criterion(outputs, labels)
+            # metric = metric_func(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if step % 10 == 0:
+                print("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
+                      .format(epoch + 1, num_epochs, step + 1, total_step, loss.item()))
+                # print("Metric:{.4f}".format(metric.item()/step))
+        if (epoch + 1) % 3 == 0:
+            curr_lr /= 3
+            update_lr(optimizer, curr_lr)
+    # print(model.state_dict().keys())
+    torch.save(model.state_dict(), args.modeldir)
+
+if __name__ == "__main__":
+    main()
